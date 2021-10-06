c43184ca76d711e6428f995de16b16bc1aee7c93
david <daviher079@gmail.com>
2021-10-06 20:08:56 +0200

    Primeros pasos

---
+++ codigo/script.js
@@ -0,0 +1,10 @@
+
+function cambiaParrafo()
+{
+    var textoDelParrafo= document.getElementById('prueba');
+    textoDelParrafo.innerHTML="Viva el Betissssss";
+} 
+
+document.getElementById('boton').addEventListener("click",cambiaParrafo,false);
+
+
